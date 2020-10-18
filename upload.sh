7a559ed1fe2b7692233aaa33f2d691d3e3d226cc
andrisstuks <andris.stuks@gmail.com>
2020-10-18 23:08:34 +0300

    18-10-2020

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ieliek="$(date '+%d-%m-%Y')"
-git init
-git config user.name "andrisstuks"
+
+git config --global user.email andris.stuks@gmail.com
 git add .
 git commit -m "$ieliek"
 git push origin master
