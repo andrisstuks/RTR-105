a767acaf45c204941bc2b0cdd89a13049aa9d5ad
andrisstuks <andris.stuks@gmail.com>
2020-10-31 15:34:41 +0200

    31-10-2020

--- relay_test_log.py
+++ relay_test_log.py
@@ -5,11 +5,16 @@ import time
 from datetime import timezone, timedelta
 import datetime as dt
 import pandas as pd
+import urllib.request
+import urllib.parse
 
 GPIO.setmode(GPIO.BCM)
 
 global pinlist
-pinlist=[23,24]
+pinlist=[24,23]
+#for i in pinlist:
+#    GPIO.setup(i, GPIO.OUT)
+#    GPIO.output(i, GPIO.LOW)
 
 natu='http://192.168.1.241:8200/last/600261'
 top_h='http://192.168.1.241:8200/last/500664'
@@ -37,13 +42,13 @@ global all_on_counter
 global all_lightsum_lim
 global all_GPIO
 
-half_lightsum_lim=15 # Gaismas summas limits, pie kuras atslēgsies gaisma
-half_off_tresh=400 # Izslēgšanās limits
-half_on_tresh=280 # atpakaļieslēgšanās limits
+half_lightsum_lim=28 # Gaismas summas limits, pie kuras atslēgsies gaisma
+half_off_tresh=500 # Izslēgšanās limits
+half_on_tresh=350 # atpakaļieslēgšanās limits
 
-all_lightsum_lim=12 #
-all_off_tresh=120 # Izslēgšanās limits
-all_on_tresh=40
+all_lightsum_lim=26 #
+all_off_tresh=300 # Izslēgšanās limits
+all_on_tresh=180
 
 half_light_status_on=True # Mākslīgo gaismu -lampu ielēgšanās status
 half_GPIO=0
@@ -51,7 +56,7 @@ all_light_status_on=True # Mākslīgo gaismu -lampu ielēgšanās status
 all_GPIO=0
 
 nat_compare=0
-lightsum=0#  kopējā gaismas summa
+lightsum=12 #  kopējā gaismas summa
 art_umol=0.0102 # Akumulētā gaismas summa pie 60sek darbības, jo uzstādīti 170
 natk=60 # sekundes minūtē
 on_off_lim=15 # minūtes, cik ilgi gaismai jāatdziest un jāuzsilst
@@ -74,12 +79,12 @@ global half_off_end_time
 global all_on_start_time
 global all_off_end_time
 
-start_hour=5 # Stunda cikos sākam gaismot -
-start_minute=56 # Minūte cikos sākam gaismot
-end_hour=5 #Stunda, ckios beidzam gaismot
+start_hour=22 # Stunda cikos sākam gaismot -
+start_minute=00 # Minūte cikos sākam gaismot
+end_hour=16 #Stunda, ckios beidzam gaismot
 end_minute=59  #Minūte, kurā beidzam stundas robežās gaismot
-all_start_delay=1 # 100% ieslēgšanās aizture
-all_end_delay=1 # 100% izslēgšanās pirms visu lampu izslēgšanās
+all_start_delay=30 # 100% ieslēgšanās aizture
+all_end_delay=15 # 100% izslēgšanās pirms visu lampu izslēgšanās
 
 # Šos neaiztiekam - tie ir laika logi
 half_on_start_time=0
@@ -142,19 +147,34 @@ def counter_reset():
         daily_artifical_range_full_end()
         pinflip()
     date=dt.datetime.now().isoformat()
-    finame="/home/juska/log/"+date[5:10]+".txt"
+    finame="/home/juska/log/"+date[5:10]+"_n.txt"
 
 def startup():
     global notachii
     global finame
+    global half_on_start_time
+    global half_off_end_time
+    now=dt.datetime.now().time().isoformat()
     if notachii==0:
         notachii+=1
-        daily_artifical_range_half()
-        daily_artifical_range_full_start()
-        daily_artifical_range_full_end()
-        pinflip()
+        if start_hour > int(now[0:2]):
+            daily_artifical_range_half()
+            """
+            Pēc sistēmas restarta, ja restarta periods iekrīt aktīvajā gaismošanas periodā, sistēma uzstāda gaismas ieslēgšanas laiku kā nākamo atbilstošo ieslēgšanās periodu  - tātad nākamajā dienā.
+            Lai to novērstu, ar timedelta pārbīdam jau aktivizēto ieslēgšanas un izslēgšanas periodu par diennakti atpakaļ, un attiecīgi esam nepieciešamajā laika periodā un gaisma ieslēgsies.
+            """
+            half_on_start_time=half_on_start_time - timedelta(days=1)
+            half_off_end_time = half_off_end_time - timedelta(days=1)
+            daily_artifical_range_full_start()
+            daily_artifical_range_full_end()
+            pinflip()
+        else:
+            daily_artifical_range_half()
+            daily_artifical_range_full_start()
+            daily_artifical_range_full_end()
+            pinflip()
     date=dt.datetime.now().isoformat()
-    finame="/home/juska/log/"+date[5:10]+".txt"
+    finame="/home/juska/log/"+date[5:10]+"_n.txt"
 
 def get_data(url):
     resp=requests.get(url)
@@ -251,6 +271,10 @@ def all_on_off_status():
         all_on_counter+=1
         all_off_counter=0
 
+# daily_artifical_range_half()
+# daily_artifical_range_full_start()
+# daily_artifical_range_full_end()
+
 def is_in_range(start,end):
     now=pd.to_datetime(dt.datetime.now().isoformat())
     return start<=now<=end
@@ -297,6 +321,25 @@ def GPIO_setmode_all():
     else:
         GPIO.output(all_GPIO, GPIO.LOW)
 
+# Papildinam ar text2reach sms moduli ar paziņojumu par kļūdu
+def sendSMS(apikey, numbers, sender, message):
+    params = {
+        'api_key': apikey,
+        'from':sender,
+        'phone': numbers,
+        'message' : message,
+        'type': 'txt',
+        'unicode': True,
+        # 'timestamp':0,
+        # 'report_url':'http://www.yoursite.com/sms/report/handler/',
+        # 'expires':0,
+        # 'schedule':0,
+        # 'blacklist':False
+        }
+    f = urllib.request.urlopen('https://api.text2reach.com/sms/send?'
+        + urllib.parse.urlencode(params))
+    return (f.read(), f.code)
+
 def write_log():
     global lightsum
     global nat_compare
@@ -315,7 +358,8 @@ def write_log():
     global all_on_counter
     global all_lightsum_lim
     global all_GPIO
-    inwrite=str(lightsum) + ','+ str(nat_compare)+','+ str(half_light_status_on)+','+ str(half_off_tresh)+','+ str(half_off_counter)+','+ str(half_on_tresh)+','+ str(half_on_counter)+','+ str(half_GPIO)+','+ str(all_light_status_on)+','+ str(all_off_tresh)+','+ str(all_on_counter)+','+ str(all_GPIO)
+    laiks=dt.datetime.now().isoformat()
+    inwrite=laiks +','+str(lightsum) + ','+ str(nat_compare)+','+ str(half_light_status_on)+','+ str(half_off_tresh)+','+ str(half_off_counter)+','+ str(half_on_tresh)+','+ str(half_on_counter)+','+ str(half_GPIO)+','+ str(all_light_status_on)+','+ str(all_off_tresh)+','+ str(all_on_counter)+','+ str(all_GPIO)
     with open(finame, 'a') as f:
         f.writelines("%s\n" % inwrite)
 
@@ -330,6 +374,12 @@ try:
         GPIO_setmode_half()
         GPIO_setmode_all()
         write_log()
+        time.sleep(60)
 except (KeyboardInterrupt, SystemExit):
     GPIO.cleanup()
+    api_key= "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" # 32 zīmju api key
+    phonelist=00000000000 # telefona numurs ar valsts kodu bez 00 un +
+    sender='random_name' # reģistrētais sūtītāja vārds
+    message='lights down'
+    sendSMS(api_key,phonelist,sender,message)
     print("Keyboard interrupt exception caught")
