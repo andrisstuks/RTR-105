df92b517dc46d1b5f9db68357ac12bee3d0f1f4c
andrisstuks <andris.stuks@gmail.com>
2020-12-02 14:22:04 +0200

    02-12-2020

--- grapher_gnu.c
+++ grapher_gnu.c
@@ -1,8 +1,19 @@
- /* fputc example: alphabet writer */
+ /*Ar šo ģenerēsim teksta failu atēlošanai */
 #include <stdio.h>
 
 int main ()
 {
+
+int a=0; //int a definēšana un vērtība
+int b=0; // int b definēšana un piešķiršana
+int rangelen=0;
+
+printf("Lūdzu ievadiet A robežu x:\n");
+scanf ("%d", &a);
+printf("Lūdzu ievadi B robežu x:\n");
+scanf("%d", &b);
+printf("Lūdzu ievadiet punktu skaitu, ne mazāku par 2:\n");//Jo vienkārši nevar būt mazāk par 2 punktiem, jo jau ir sākumpunkts un beigu punkts
+
   FILE * pFile;
   char c;
 
